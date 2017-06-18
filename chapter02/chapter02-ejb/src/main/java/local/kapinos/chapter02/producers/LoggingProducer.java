051cebb63886a15901f95eac712b41a06ab22e11
Evgeniy Kapinos <evgeniy.kapinos@gmail.com>
2017-06-18 13:36:57 +0300

    Added Dispose

--- chapter02/chapter02-ejb/src/main/java/local/kapinos/chapter02/producers/LoggingProducer.java
+++ chapter02/chapter02-ejb/src/main/java/local/kapinos/chapter02/producers/LoggingProducer.java
@@ -1,13 +1,25 @@
 package local.kapinos.chapter02.producers;
 
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import javax.enterprise.inject.Disposes;
 import javax.enterprise.inject.Produces;
 import javax.enterprise.inject.spi.InjectionPoint;
+import javax.inject.Singleton;
 
+// We need any bean for dispose. Otherwise next will appear in a log:
+// Severe: No valid EE environment for injection of local.kapinos.chapter02.producers.LoggingProducer
+@Singleton
 public class LoggingProducer {
+
 	@Produces
-	private Logger createLogger(InjectionPoint injectionPoint) {
+	public Logger createLogger(InjectionPoint injectionPoint) {
 		return Logger.getLogger(injectionPoint.getMember().getDeclaringClass().getName());
+	}
+	
+	public void disposeLogger(@Disposes Logger logger)
+	{
+		logger.log(Level.WARNING, "disposeLogger " + logger.getName());
 	}
 }

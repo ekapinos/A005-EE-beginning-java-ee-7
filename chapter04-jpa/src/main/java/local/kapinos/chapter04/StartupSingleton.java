a4a2127e309c66fd5c403d3e48cb728132612362
Evgeniy Kapinos <evgeniy.kapinos@gmail.com>
2017-12-05 23:46:44 +0200

    Added @NamedQuery and ConstraintViolationException

--- chapter04-jpa/src/main/java/local/kapinos/chapter04/StartupSingleton.java
+++ chapter04-jpa/src/main/java/local/kapinos/chapter04/StartupSingleton.java
@@ -1,5 +1,6 @@
 package local.kapinos.chapter04;
 
+import java.util.List;
 import java.util.logging.Logger;
 
 import javax.annotation.PostConstruct;
@@ -7,6 +8,7 @@ import javax.ejb.Singleton;
 import javax.ejb.Startup;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.validation.ConstraintViolationException;
 
 import local.kapinos.chapter04.model.Book;
 
@@ -23,8 +25,19 @@ public class StartupSingleton {
 	public void postConstruct() {
 		logger.warning("Start");
 		
+		// See log for SQL queries
 		em.persist(new Book("Title", 12.0f, "Description", "isbn", 10, false));
+		em.persist(new Book("H2G2", 13.0f, "Description H2G2", "isbn-H2G2", 11, true));
 		
+		List<Book> books = em.createNamedQuery("findBookH2G2", Book.class).getResultList();
+		logger.info("Named Query book: " + books);
+		
+		try{
+		  em.persist(new Book("Title", 12.0f, "too short", "isbn", 10, false));
+		}catch(ConstraintViolationException e){
+			logger.info("Expected fail: " + e.getConstraintViolations());
+		}
+
 		logger.warning("End");
 	}
 
--- chapter04-jpa/src/main/java/local/kapinos/chapter04/model/Book.java
+++ chapter04-jpa/src/main/java/local/kapinos/chapter04/model/Book.java
@@ -3,12 +3,18 @@ package local.kapinos.chapter04.model;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+@NamedQueries({ 
+	@NamedQuery(name = "findAllBooks", query = "SELECT b FROM Book b"),
+	@NamedQuery(name = "findBookH2G2", query = "SELECT b FROM Book b WHERE b.title ='H2G2'") })
 @Entity
 public class Book {
-	@Id @GeneratedValue
+	@Id
+	@GeneratedValue
 	private Long id;
 	@NotNull
 	private String title;
@@ -18,11 +24,12 @@ public class Book {
 	private String isbn;
 	private Integer nbOfPage;
 	private Boolean illustrations;
-	
+
 	@SuppressWarnings("unused")
-	private Book()	{
-		
+	private Book() {
+
 	}
+
 	public Book(String title, Float price, String description, String isbn, Integer nbOfPage, Boolean illustrations) {
 		super();
 		this.title = title;
@@ -32,46 +39,69 @@ public class Book {
 		this.nbOfPage = nbOfPage;
 		this.illustrations = illustrations;
 	}
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getTitle() {
 		return title;
 	}
+
 	public void setTitle(String title) {
 		this.title = title;
 	}
+
 	public Float getPrice() {
 		return price;
 	}
+
 	public void setPrice(Float price) {
 		this.price = price;
 	}
+
 	public String getDescription() {
 		return description;
 	}
+
 	public void setDescription(String description) {
 		this.description = description;
 	}
+
 	public String getIsbn() {
 		return isbn;
 	}
+
 	public void setIsbn(String isbn) {
 		this.isbn = isbn;
 	}
+
 	public Integer getNbOfPage() {
 		return nbOfPage;
 	}
+
 	public void setNbOfPage(Integer nbOfPage) {
 		this.nbOfPage = nbOfPage;
 	}
+
 	public Boolean getIllustrations() {
 		return illustrations;
 	}
+
 	public void setIllustrations(Boolean illustrations) {
 		this.illustrations = illustrations;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Book [id=").append(id).append(", title=").append(title).append(", price=").append(price)
+				.append(", description=").append(description).append(", isbn=").append(isbn).append(", nbOfPage=")
+				.append(nbOfPage).append(", illustrations=").append(illustrations).append("]");
+		return builder.toString();
 	}
 }
